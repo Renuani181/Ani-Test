e347e3f08d59e9e80e662d0803d70375051fc1e7
Goutham Kumar Yadavalli <lone@Gouthams-MBP.attlocal.net>
2022-02-14 12:35:20 -0600

    Second version

--- mycode.py
+++ mycode.py
@@ -1,2 +1,21 @@
-r = 5
-print("Area of Circle is: ", 3.14*r*r)
+def squareArea():
+    s = float(input("Enter side length of square: "))
+    print("Area of Square is: ", s*s)
+    
+
+def circleArea():
+    r = float(input("Enter radius of circle: "))
+    print("Area of Circle is: ", 3.14*r*r)
+    
+if __name__=="__main__":
+    while(1):
+        selection = int(input("Enter 1 for Square and 2 for Circle: "))
+        if(selection==1):
+            squareArea()
+            break
+        elif(selection==2):
+            circleArea()
+            break
+        else:
+            print("Invalid input try again.")
+            continue
